27cebb01d00d54178cd8a8522d1abaf5b974fc05
Anthony Jakob <anthony.jakob@outlook.com>
2019-12-16 11:12:52 +0100

    All files for report

--- launch-defreeze-100.sh
+++ launch-defreeze-100.sh
@@ -18,7 +18,7 @@ source /ssoft/spack/bin/slmodules.sh -s x86_E5v2_Mellanox_GPU
 module load gcc cuda cudnn python mvapich2
 source /home/amjakob/venvs/tensorflow-1.9/bin/activate
 
-time srun python train_classification_tl.py --ckpt_load=keras_swisspv_untrained.h5 --epochs=1000 --batch_size=128 --epochs_ckpt=5 --train_set=train.pickle --test_set=test.pickle --validation_split=0.25 --from_scratch=True --skip_train=False --skip_test=False --optimizer=rmsprop --loss=binary_crossentropy --verbose=1
+time srun python train_classification_tl_ft.py --ckpt_load=keras_swisspv_untrained.h5 --epochs=500 --batch_size=128 --epochs_ckpt=5 --train_set=train.pickle --test_set=test.pickle --validation_split=0.25 --from_scratch=True --skip_train=False --skip_test=False --optimizer=rmsprop --loss=binary_crossentropy --fine_tuning=True --fine_tune_layers=100 --verbose=1
 
 deactivate
 
